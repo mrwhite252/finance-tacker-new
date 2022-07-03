703ad91ad3048a0f3d5905c38c170713ffc536fc
David Bai <baiminghan@gmail.com>
2022-07-03 17:02:59 +1200

    conditionally rendering part of the template on the navbar depends on the login conditions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 
 // styles
@@ -11,16 +12,26 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
 
     const { logout } = useLogout();
+    const { user } = useAuthContext();
     return (
         <nav className={styles.navbar}>
             <ul>
                 <li className={styles.title}>My Money</li>
-                <li><Link to="login">Login</Link></li>
-                <li><Link to="signup">Signup</Link></li>
 
-                <li>
-                    <button className="btn" onClick={logout}>Logout</button>
-                </li>
+                {!user && (<>
+                    <li><Link to="login">Login</Link></li>
+                    <li><Link to="signup">Signup</Link></li>
+                </>)}
+
+
+                {user && (
+                    <>
+                        <li>Hello, {user.displayName}</li>
+                        <li>
+                            <button className="btn" onClick={logout}>Logout</button>
+                        </li>
+                    </>
+                )}
             </ul>
         </nav>
     )
