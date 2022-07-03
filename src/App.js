3028975a92a3a6bbce40c1a209b51a1992485faa
David Bai <baiminghan@gmail.com>
2022-07-03 19:37:16 +1200

    setup the router guard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { useAuthContext } from './hooks/useAuthContext';
 
 // pages and components
@@ -11,7 +11,7 @@ import Navbar from './components/Navbar';
 
 function App() {
 
-  const { authIsReady } = useAuthContext()
+  const { authIsReady, user } = useAuthContext()
   return (
     <div className="App">
       {authIsReady && (
@@ -19,13 +19,16 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/">
-              <Home />
+              {!user && <Redirect to="/login" />}
+              {user && <Home />}
             </Route>
             <Route path="/login">
-              <Login />
+              {user && <Redirect to="/" />}
+              {!user && <Login />}
             </Route>
             <Route path="/signup">
-              <Signup />
+              {user && <Redirect to="/" />}
+              {!user && <Signup />}
             </Route>
           </Switch>
         </BrowserRouter>
