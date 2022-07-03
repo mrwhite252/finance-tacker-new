02b80506d410021879ced4183ad40c98ab6881f1
David Bai <baiminghan@gmail.com>
2022-07-03 16:45:03 +1200

    use the useLogin hook

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
 
 // import styles
 import styles from "./Login.module.css";
@@ -7,10 +8,11 @@ export default function Login() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const { login, error, isPending } = useLogin();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email, password);
+        login(email, password);
     }
     return (
         <form onSubmit={handleSubmit} className={styles['login-form']}>
@@ -29,7 +31,9 @@ export default function Login() {
                     value={password}
                 />
             </label>
-            <button className="btn">Login</button>
+            {!isPending && <button className="btn">Login</button>}
+            {isPending && <button className="btn" disabled>loading</button>}
+            {error && <p>{error}</p>}
         </form>
     )
 }
