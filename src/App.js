ed01ae7792a75024448fa1aac3b2b85b57d772a3
David Bai <baiminghan@gmail.com>
2022-07-03 19:26:41 +1200

    check authIsReady status before rendering the whole app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-
+import { useAuthContext } from './hooks/useAuthContext';
 
 // pages and components
 import Home from './pages/home/Home';
@@ -10,24 +10,26 @@ import Navbar from './components/Navbar';
 
 
 function App() {
+
+  const { authIsReady } = useAuthContext()
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-
-      </BrowserRouter>
-
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
 
     </div>
   );
